a7f7cd3382c517427ae0feff4776c1d42ec43758
Ralph Lange <ralph.lange@gmx.de>
2025-01-05 18:02:30 +0100

    WIP run gtests under RTEMS

--- sampleApp/mixdGtestEpicsSrc/rtemsTestHarness.cpp
+++ sampleApp/mixdGtestEpicsSrc/rtemsTestHarness.cpp
@@ -6,10 +6,23 @@
 * in file LICENSE that is included with this distribution.
 \*************************************************************************/
 
-extern void epicsRunSampleTests(void);
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+#include <tap.h>
+
+extern "C" void epicsRunSampleTests(void);
 
 int main(int argc, char **argv)
 {
+    bool addTapListener = true;
+
     epicsRunSampleTests();  /* calls epicsExit(0) */
-    return 0;
+
+    testing::InitGoogleMock(&argc, argv);
+
+    if (addTapListener) {
+        testing::TestEventListeners& listeners = testing::UnitTest::GetInstance()->listeners();
+        listeners.Append(new tap::TapListener());
+    }
+    return RUN_ALL_TESTS();
 }
